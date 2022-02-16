910b7857058c6f9c76dbcfbe8764aa579b60e3a9
Ninon85 <88130318+Ninon85@users.noreply.github.com>
2022-02-16 18:15:27 +0100

    install react

--- backend/routes/routes/comment.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const commentCtrl = require("../controller/comment");
-router.post("/:id/comment");
-router.put("/:id/comment");
-router.delete("/:id/comment");
-module.exports = router;
--- backend/routes/routes/like.js
+++
@@ -1,4 +0,0 @@
-const router = require("express").Router();
-const likeCtrl = require("../controller/like");
-router.post("/:id/like");
-module.exports = router;
